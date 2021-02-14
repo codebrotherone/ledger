04e0dd6a95dac45f87597e07cf7da2657598de17
Jae Han <jaehan@Jaes-MBP.attlocal.net>
2021-02-14 12:25:37 -0600

    added querybuilder file to add more robust query handling

---
+++ insurance/query_builder.py
@@ -0,0 +1,14 @@
+"""
+Simple class for constructing queries for interacting with the Sqlite3 DB
+This db will contain a table called policies, and the below class will construct
+queries based on what is passed into the GET call for the insurance.query API
+
+TODO(jae.han):
+    - Ideally a query builder would be used, since SQLite does not excel at
+    dynamic querying.
+    - However due to time constraints, I was not able to finish this portion.
+"""
+
+
+class QueryBuilder:
+    pass
